8fad04fc9b66c9797d2cda9aef6c6f3e9b83e48c
jangyoungdo <mapl003@naver.com>
2017-08-01 11:41:12 +0900

    first commit

---
+++ app/src/main/java/graduateproject/com/twentyquestions/activity/BaseActivity.java
@@ -0,0 +1,25 @@
+package graduateproject.com.twentyquestions.activity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by mapl0 on 2017-08-01.
+ */
+
+public class BaseActivity extends Activity {
+
+    public static Context mContext;
+
+    public BaseActivity() {
+
+        mContext = this;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/graduateproject/com/twentyquestions/network/DBSI.java
@@ -0,0 +1,8 @@
+package graduateproject.com.twentyquestions.network;
+
+/**
+ * Created by mapl0 on 2017-08-01.
+ */
+
+public class DBSI {
+}
